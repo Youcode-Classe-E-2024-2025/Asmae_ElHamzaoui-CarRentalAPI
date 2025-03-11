68e4f19c246061eb834d6b61858fe15048b60a74
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-11 23:49:22 +0000

    Ajout des routes publiques dans le fichier api.php

--- CarRentalAPI/routes/api.php
+++ CarRentalAPI/routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\RentalController;
+use App\Http\Controllers\PaymentController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Routes publiques 
+Route::post('register', [UserController::class, 'store']); // Inscription
+Route::post('login', [UserController::class, 'login']);   // Connexion
+Route::get('cars', [CarController::class, 'index']);      // Liste des voitures disponibles
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
