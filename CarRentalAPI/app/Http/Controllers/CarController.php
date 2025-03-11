ee1ffb2a75f0a62a5cb5ec845d0e08a6d56b9872
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-11 23:26:06 +0000

    implémentation des fonctionnalités de crud dans le controleur Car

--- CarRentalAPI/app/Http/Controllers/CarController.php
+++ CarRentalAPI/app/Http/Controllers/CarController.php
@@ -7,79 +7,42 @@ use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // Lire toutes les voitures
     public function index()
     {
-        //
+        return Car::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    // Créer une voiture
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'brand' => 'required|string',
+            'model' => 'required|string',
+            'year' => 'required|integer',
+            'price_per_day' => 'required|numeric'
+        ]);
+
+        return Car::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Car  $car
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Car $car)
+    // Lire une voiture spécifique
+    public function show($id)
     {
-        //
+        return Car::findOrFail($id);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Car  $car
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Car $car)
+    // Mettre à jour une voiture
+    public function update(Request $request, $id)
     {
-        //
+        $car = Car::findOrFail($id);
+        $car->update($request->all());
+        return $car;
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Car  $car
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Car $car)
+    // Supprimer une voiture
+    public function destroy($id)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Car  $car
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Car $car)
-    {
-        //
+        return Car::destroy($id);
     }
 }
