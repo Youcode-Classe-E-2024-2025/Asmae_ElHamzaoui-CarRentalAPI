9f9b568d20a29c46c2242ac2a9380eef8606a92c
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-11 23:22:51 +0000

    ajout des fillable ainsi que les relations possibles dans le model Rental

--- CarRentalAPI/app/Models/Rental.php
+++ CarRentalAPI/app/Models/Rental.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Rental extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'car_id', 'start_date', 'end_date', 'total_price'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function car()
+    {
+        return $this->belongsTo(Car::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
 }
+
