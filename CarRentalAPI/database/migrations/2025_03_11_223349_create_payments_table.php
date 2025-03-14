64f8de15c73a90859f51fae7279931a159ad13aa
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-14 06:27:10 +0000

    ajout des colonnes transaction_id et status dans la migration payments

--- CarRentalAPI/database/migrations/2025_03_11_223349_create_payments_table.php
+++ CarRentalAPI/database/migrations/2025_03_11_223349_create_payments_table.php
@@ -4,22 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up()
+return new class extends Migration
+{
+
+    public function up(): void
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('rental_id')->constrained()->onDelete('cascade');
-            $table->decimal('amount', 8, 2);
-            $table->string('payment_method');
-            $table->date('payment_date');
+            $table->decimal('amount', 10, 2);
+            $table->enum('payment_method', ['credit_card', 'debit_card', 'paypal', 'cash']);
+            $table->string('transaction_id')->nullable();
+            $table->enum('status', ['pending', 'completed', 'failed', 'refunded'])->default('pending');
             $table->timestamps();
         });
     }
 
-    public function down()
+
+    public function down(): void
     {
         Schema::dropIfExists('payments');
     }
 };
-
