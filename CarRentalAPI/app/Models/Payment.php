c0709a1c6d838ce9867a05569ff73a6028d35abb
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-14 06:34:03 +0000

    implémentaion du modèl Payment avec les relations principales

--- CarRentalAPI/app/Models/Payment.php
+++ CarRentalAPI/app/Models/Payment.php
@@ -5,6 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Payment
+ * @package App\Models
+ *
+ * @property int $id
+ * @property int $rental_id
+ * @property float $amount
+ * @property string $payment_method
+ * @property string $payment_date
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
+
 class Payment extends Model
 {
     use HasFactory;
