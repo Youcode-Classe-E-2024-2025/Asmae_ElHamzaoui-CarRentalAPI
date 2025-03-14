ac2c09e320539c482790567fd8a22e193c31125a
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-14 06:32:46 +0000

    implémentaion du modèl car avec les relations principales

--- CarRentalAPI/app/Models/Car.php
+++ CarRentalAPI/app/Models/Car.php
@@ -5,6 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Car
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $brand
+ * @property string $model
+ * @property int $year
+ * @property float $price_per_day
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
+
 class Car extends Model
 {
     use HasFactory;
