63ce8d4fe618e889df9b2fbac0b6ac16de9dcca8
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-14 06:33:39 +0000

    implémentaion du modèl Rental avec les relations principales

--- CarRentalAPI/app/Models/Rental.php
+++ CarRentalAPI/app/Models/Rental.php
@@ -5,6 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * Class Rental
+ * @package App\Models
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $car_id
+ * @property string $start_date
+ * @property string $end_date
+ * @property float $total_price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
+
 class Rental extends Model
 {
     use HasFactory;
