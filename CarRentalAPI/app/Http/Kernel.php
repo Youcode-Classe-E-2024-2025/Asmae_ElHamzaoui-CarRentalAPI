80485cc4e28c7d058b708ea176ece45efba40dd9
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-03-14 11:24:23 +0000

    ajout des dépendances de sunctum dans le fichier kernel.php

--- CarRentalAPI/app/Http/Kernel.php
+++ CarRentalAPI/app/Http/Kernel.php
@@ -37,9 +37,8 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // This ensures Sanctum is working properly
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
@@ -64,4 +63,9 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $middlewareAliases = [
+        'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    ];
+    
 }
